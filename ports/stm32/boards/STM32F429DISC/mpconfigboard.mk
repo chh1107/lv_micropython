4132e82c216041058e0b8e08f6ae974986924b2c
Christian Hinrichs <christian.hinrichs@btc-ag.com>
2023-03-20 17:27:49 +0100

    removed unused flag

--- ports/stm32/boards/STM32F429DISC/mpconfigboard.mk
+++ ports/stm32/boards/STM32F429DISC/mpconfigboard.mk
@@ -9,8 +9,5 @@ TEXT1_ADDR = 0x08020000
 MICROPY_VFS_LFS2 = 1
 # CH: Enable LVGL binding
 MICROPY_PY_LVGL = 1
-# CH: Configure the correct hardware components for the latest board revision
-# (see stm32f429i_discovery.h from lib/STM32CubeF4 package)
-USE_STM32F429I_DISCOVERY_REVD = 1
 # CH: Configure include locations for frozen MicroPython modules
 FROZEN_MANIFEST ?= ../../lib/rt_lab_micropython/manifest.py
